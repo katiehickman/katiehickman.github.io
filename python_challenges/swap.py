a5b66780507c79237e6ad381faa85b17430d556b
katiehickman <katie.hickman2704@gmail.com>
2022-03-13 17:54:59 -0700

    fixed christmas and swap

--- python_challenges/swap.py
+++ python_challenges/swap.py
@@ -1,20 +1,10 @@
-#def swapnumbers(age1, age2):
-  #  if age1 > age2:
- #       temp = age1
- #      age1 = age2
- #       age2 = temp
-  #  return age1, age2
+def swap(age1, age2):
+    temp = age1
+    age1 = age2
+    age2 = temp
+    print("After Swap",(age1, age2))
+number1 = int(input(" Please Enter the First number : "))
+number2 = int(input(" Please Enter the Second number : "))
+print("Before Swap",(number1, number2))
+swap(number1, number2)
 
-def swap_input():
-    list = []
-    age1 = input("Enter the first number:")
-    age2 = input("Enter the second number:")
-    def swap(num1, num2):
-        if age1 > age2:
-            list.append(age2)
-            list.append(age1)
-        if age1 < age2:
-            list.append(age1)
-            list.append(age2)
-    swap(age1,age2)
-    print("result", list)
